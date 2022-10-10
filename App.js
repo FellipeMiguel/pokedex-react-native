532b14221d05e527dd4ad592798c320d597c0872
Fellipe M Dino <fellipemiguel1@icloud.com>
2022-10-10 15:13:53 -0300

    Inicio da primeira página

--- App.js
+++ App.js
@@ -10,7 +10,21 @@ export default function App() {
       colors={["#56B4FF", "#FFF"]}
       style={styles.container}
     >
-      <View style={styles.container}></View>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.telaInicial}>
+          <View style={styles.logo}>
+            <Image
+              style={styles.logoImagem}
+              source="./assets/images/Pokémon-Logo.png"
+            />
+          </View>
+          <View style={styles.texos}>
+            <Text style={styles.h1}>Hello There!</Text>
+            <Text style={styles.h2}>Welcome to the world of POKEMON!</Text>
+          </View>
+        </View>
+      </View>
     </LinearGradient>
   );
 }
